97e08ab3dadbebab4d089012e63bc8c82e131524
meb312 <meb312@gmail.com>
2015-04-22 15:27:17 -0400

    Assignment 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions first produce a set of functions to be used as inputs into
+## cacheSolve, which returns the inverse of input matrix if makeCacheMatrix has not
+## been rerun, otherwise, it calculates a new inverse matrix for the new input matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the functions used as inputs to cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	m<-NULL
+	set<-function(y){
+		x<<-y
+		m<<-NULL
+	}
+	get<-	function()x
+	setinverse<-function(solve)m<<-solve
+	getinverse<-function()m
+	list(set=set, get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve takes the objects from makeCacheMatrix as inputs, checks if m exists
+## and is not NULL; if so, it simply returns the stored value of m(the inveset matrix).
+## If m is NULL, meaning makeCacheMatrix has been re-run and m reset, then cacheSolve
+## calculate the new inverse matrix for m.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+
 }
